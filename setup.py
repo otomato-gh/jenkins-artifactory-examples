bd21874650098748ee941f721929d3dc29b69458
Anton Weiss <anton@otomato.io>
2021-10-15 10:35:28 +0300

    Package details otomato

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from setuptools import setup
 
 setup(
-    name='jfrog-python-example',
+    name='otomato-python-example',
     version='1.0',
     description='Project example for building Python project with JFrog products',
-    author='JFrog',
-    author_email='jfrog@jfrog.com',
-    url='https://github.com/jfrog/project-examples',
+    author='Otomato',
+    author_email='contact@otomato.com',
+    url='https://github.com/otomato-gh/jenkins-artifactory-examples',
     packages=['pythonExample'],
     install_requires=['PyYAML>3.11', 'nltk'],
 )
